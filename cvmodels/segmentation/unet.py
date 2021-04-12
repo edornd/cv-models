ec631f4795e1ada8472e8b4badb3947979531de9
Edoardo Arnaudo <edoardo.arn@gmail.com>
2021-04-12 17:05:13 +0000

    Rewrite UNet to account for bilinear upsampling

--- cvmodels/segmentation/unet.py
+++ cvmodels/segmentation/unet.py
@@ -1,5 +1,62 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
+
+
+def convolution_block(in_channels: int, out_channels: int, mid_channels: Optional[int] = None) -> nn.Module:
+    """Creates a sequential block, containing two composite layers in series with
+    (convolution, batch norm, relu activation).
+
+    :param in_channels: number of input channels for the current block (2nd dimension of the batch)
+    :type in_channels:  int
+    :param out_channels: number of output channels for the current block
+    :type out_channels: int
+    :param mid_channels: number of output channels for the middle block
+    :type mid_channels: int
+    :return:            Sequential module containing a double 3x3 convolution with BN and ReLU activations
+    :rtype:             nn.Module
+    """
+    mid_channels = mid_channels or out_channels
+    return nn.Sequential(nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+                         nn.BatchNorm2d(num_features=mid_channels),
+                         nn.ReLU(inplace=True),
+                         nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+                         nn.BatchNorm2d(num_features=out_channels),
+                         nn.ReLU(inplace=True))
+
+
+class EncoderBlock(nn.Module):
+    """Basic block to define the encoding basic block with pooling and double convolution.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.conv_block = convolution_block(in_channels=in_channels, out_channels=out_channels)
+        self.max_pool = nn.MaxPool2d(kernel_size=2)
+
+    def forward(self, batch: torch.Tensor):
+        x = self.max_pool(batch)
+        return self.conv_block(x)
+
+
+class DecoderBlock(nn.Module):
+    """Basic block describing the upsampling and double convolution procedure.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True):
+        super().__init__()
+        if bilinear:
+            self.upsample = nn.Sequential(nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+                                          nn.Conv2d(in_channels, out_channels, kernel_size=1))
+            self.conv_block = convolution_block(in_channels=in_channels, out_channels=out_channels)
+        else:
+            self.upsample = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+            self.conv_block = convolution_block(in_channels, out_channels)
+
+    def forward(self, batch: torch.Tensor, skip: torch.Tensor):
+        upsampled = self.upsample(batch)
+        return self.conv_block(torch.cat((skip, upsampled), dim=1))
 
 
 class UNet(nn.Module):
@@ -7,9 +64,15 @@ class UNet(nn.Module):
     The structure remains as close as possible to the paper (https://arxiv.org/abs/1505.04597), with the exception
     of the extra padding in the convolutional blocks, which helps maintaining the output shape equal to the input,
     instead of the crops proposed in the paper.
+    The bilinear upsampling is also provided as option: while not being strictly compliant, it should provide better
+    results without "checkboards" artifacts (https://distill.pub/2016/deconv-checkerboard/)
     """
 
-    def __init__(self, input_channels: int = 3, init_features: int = 64, outputs: int = 1) -> None:
+    def __init__(self,
+                 input_channels: int = 3,
+                 init_features: int = 64,
+                 outputs: int = 1,
+                 bilinear: bool = True) -> None:
         """Creates a new UNet model, with the given number of input channels, initial features and final output layers.
 
         :param input_channels: channel count in the input images, defaults to 3 for RGB
@@ -22,38 +85,16 @@ class UNet(nn.Module):
         super().__init__()
         f = init_features
         # encoder
-        self.max_pool2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder1 = self._conv_block(input_channels, f)
-        self.encoder2 = self._conv_block(f, f * 2)
-        self.encoder3 = self._conv_block(f * 2, f * 4)
-        self.encoder4 = self._conv_block(f * 4, f * 8)
-        self.encoder5 = self._conv_block(f * 8, f * 16)
-        # decoder
-        self.upscale1 = nn.ConvTranspose2d(f * 16, f * 8, kernel_size=2, stride=2)
-        self.decoder1 = self._conv_block(f * 16, f * 8)
-        self.upscale2 = nn.ConvTranspose2d(f * 8, f * 4, kernel_size=2, stride=2)
-        self.decoder2 = self._conv_block(f * 8, f * 4)
-        self.upscale3 = nn.ConvTranspose2d(f * 4, f * 2, kernel_size=2, stride=2)
-        self.decoder3 = self._conv_block(f * 4, f * 2)
-        self.upscale4 = nn.ConvTranspose2d(f * 2, f, kernel_size=2, stride=2)
-        self.decoder4 = self._conv_block(f * 2, f)
+        self.input = convolution_block(input_channels, f)
+        self.encoder1 = EncoderBlock(f, f * 2)
+        self.encoder2 = EncoderBlock(f * 2, f * 4)
+        self.encoder3 = EncoderBlock(f * 4, f * 8)
+        self.encoder4 = EncoderBlock(f * 8, f * 16)
+        self.decoder1 = DecoderBlock(f * 16, f * 8, bilinear=bilinear)
+        self.decoder2 = DecoderBlock(f * 8, f * 4, bilinear=bilinear)
+        self.decoder3 = DecoderBlock(f * 4, f * 2, bilinear=bilinear)
+        self.decoder4 = DecoderBlock(f * 2, f, bilinear=bilinear)
         self.output = nn.Conv2d(f, outputs, kernel_size=1)
-
-    def _conv_block(self, in_channels: int, out_channels: int) -> nn.Module:
-        """Creates a sequential block, containing two composite layers in series with
-        (convolution, batch norm, relu activation).
-
-        :param in_channels: number of input channels for the current block (2nd dimension of the batch)
-        :type in_channels:  int
-        :param out_channels: number of output channels for the current block
-        :type out_channels: int
-        :return:            Sequential module containing a double 3x3 convolution with BN and ReLU activations
-        :rtype:             nn.Module
-        """
-        return nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-                             nn.BatchNorm2d(num_features=out_channels), nn.ReLU(inplace=True),
-                             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-                             nn.BatchNorm2d(num_features=out_channels), nn.ReLU(inplace=True))
 
     def forward(self, batch: torch.Tensor) -> torch.Tensor:
         """Encodes the given batch of images into a semantically meaningful vector.
@@ -63,23 +104,27 @@ class UNet(nn.Module):
         :return:        tensor [batch, 1024, h, w], where h, w depend on the input dimension
         :rtype:         torch.Tensor
         """
-        print(batch.size())
-        f1 = self.encoder1(batch)
-        f2 = self.max_pool2x2(f1)
-        f2 = self.encoder2(f2)
-        f3 = self.max_pool2x2(f2)
-        f3 = self.encoder3(f3)
-        f4 = self.max_pool2x2(f3)
-        f4 = self.encoder4(f4)
-        f5 = self.max_pool2x2(f4)
-        f5 = self.encoder5(f5)
-        # decoder
-        out = self.upscale1(f5)
-        out = self.decoder1(torch.cat((out, f4), dim=1))
-        out = self.upscale2(out)
-        out = self.decoder2(torch.cat((out, f3), dim=1))
-        out = self.upscale3(out)
-        out = self.decoder3(torch.cat((out, f2), dim=1))
-        out = self.upscale4(out)
-        out = self.decoder4(torch.cat((out, f1), dim=1))
-        return self.output(out)
+        x1 = self.input(batch)
+        x2 = self.encoder1(x1)
+        x3 = self.encoder2(x2)
+        x4 = self.encoder3(x3)
+        x5 = self.encoder4(x4)
+        x = self.decoder1(x5, x4)
+        x = self.decoder2(x, x3)
+        x = self.decoder3(x, x2)
+        x = self.decoder4(x, x1)
+        return self.output(x)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+if __name__ == "__main__":
+    batch = 2
+    channels = 3
+    x = torch.rand((batch, channels, 512, 512))
+    model = UNet(bilinear=True)
+    out = model(x)
+    print(count_parameters(model))
+    print(out.size())
