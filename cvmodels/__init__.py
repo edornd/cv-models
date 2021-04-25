b4a5d485c32f6e17f26010ca37dec90a230c1523
Edoardo Arnaudo <edoardo.arn@gmail.com>
2021-04-25 08:55:51 +0000

    Add simple adaptive input from pretrained weights [#4]

--- cvmodels/__init__.py
+++ cvmodels/__init__.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn import Module
 from torch.utils import model_zoo
 
@@ -20,9 +21,22 @@ class ModuleBase(Module):
         pretrained = model_zoo.load_url(url)
         updates = {}
         current_state = self.state_dict()
-        for k, v in pretrained.items():
+        # check pretrained input channels, adapt if necessary
+        for i, (k, v) in enumerate(pretrained.items()):
             k = self._layer_mapping(k)
             if k in current_state:
+                # if it's input
+                if i == 0:
+                    pretrained_channels = v.shape[1]
+                    input_channels = current_state[k].shape[1]
+                    # if input channels are less than nominal 3, slice
+                    if input_channels < pretrained_channels:
+                        v = v[:, :pretrained_channels]
+                    # if input channels are more than 3, duplicate the first N times
+                    elif input_channels > pretrained_channels:
+                        count = input_channels - pretrained_channels
+                        additional = v[:, 0].unsqueeze(1).repeat(1, count, 1, 1)
+                        v = torch.cat((v, additional), dim=1)
                 updates[k] = v
         current_state.update(updates)
         self.load_state_dict(current_state)
